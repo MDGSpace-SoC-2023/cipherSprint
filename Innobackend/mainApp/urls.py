ea66550aef6cbd676db086820ec73279c6767b33
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-13 02:02:53 +0530

    feat:message_viewset_custom_changes

--- Innobackend/mainApp/urls.py
+++ Innobackend/mainApp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
     path('', include(router.urls)),
     path('project/project_user/',ProjectViewSet.as_view({"get":"get_project_logged_user"},name="project_1")),
+    path('message/project/<int:project_id>/',MessageViewSet.as_view({"get":"project_messages"},name='message_1')),
     
    
 ]
