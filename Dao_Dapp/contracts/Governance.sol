c4dcab7f4a892a5c952b428748cd18989c84eb79
Chitraksh <chitrakshsureka@gmail.com>
2023-12-15 20:15:17 +0530

    compile governance

--- Dao_Dapp/contracts/Governance.sol
+++ Dao_Dapp/contracts/Governance.sol
@@ -1,28 +1,33 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GovernanceToken is  ERC20Votes{
+contract Governance is ERC20, ERC20Votes, ERC20Permit {
+  uint256 public s_maxSupply = 1000000000000000000000000;
 
-    uint256 max_supply=1000000000000000000;
+  constructor() ERC20("Governance", "GT") ERC20Permit("Governance") {
+    _mint(msg.sender, s_maxSupply);
+  }
 
-    constructor()
-       ERC20("GovernanceToken","GT")
-       ERC20Permit("GovernanceToken")
-    {
-        _mint(msg.sender,max_supply);
-    }
+  // The functions below are overrides required by Solidity.
+  function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256){
+    return super.nonces(owner);
+  }
 
-    function _afterTokenTransfer(address from,address to,uint256 amount) internal override(ERC20Votes){
-        super._afterTokenTransfer(from,to,amount);
-    }
+   
+  function _update(address from,address to,uint256 value) internal virtual override (ERC20, ERC20Votes){
+    return super._update(from,to,value);
+  }
+ 
 
-    function _mint(address to ,uint256 amount) internal override(ERC20Votes){
-        super._mint(to,amount);
-    }
+  function _mint(address to, uint256 amount) internal override(ERC20) {
+    super._mint(to, amount);
+  }
 
-    function _burn(address account,uint256 amount) internal override(ERC20Votes){
-        super._burn(account,amount);
-    }
+  function _burn(address account, uint256 amount) internal override(ERC20) {
+    super._burn(account, amount);
+  }
 }
