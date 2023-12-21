34faa99af75f7e206c289fa3d9a753eff95c9ceb
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-22 03:34:35 +0530

    bug:public_host_removed

--- new_frontend/vue.config.js
+++ new_frontend/vue.config.js
@@ -2,24 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 
-module.exports = {
-  transpileDependencies: true,
-  devServer: {
-    // Fixing issue with WDS disconnected and sockjs network error
-    proxy: 'http://localhost:8000',
-    host: '0.0.0.0',
-    public: '0.0.0.0:8080',
-    disableHostCheck: true,
-    // End of fix
-  }
-}
-
-
-// module.exports = defineConfig({
+// module.exports = {
 //   transpileDependencies: true,
 //   devServer: {
+//     // Fixing issue with WDS disconnected and sockjs network error
 //     proxy: 'http://localhost:8000',
-//     port: 8080,
-//     public: 'https://localhost:5001'
+//     host: '0.0.0.0',
+//     public: '0.0.0.0:8080',
+//     disableHostCheck: true,
+//     // End of fix
 //   }
-// })
+// }
+
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'http://localhost:8000',
+    port: 8080,
+  }
+})
