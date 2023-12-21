bda11b4d3c0b70ccdad9fa5483b4e1de7f26b3ea
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-22 03:16:23 +0530

    feat:Login_back_front_merge

--- Innobackend/mainApp/views/logins.py
+++ Innobackend/mainApp/views/logins.py
@@ -8,38 +8,42 @@ from mainApp.serializers import User_Serializer
 from rest_framework.parsers import JSONParser
 
 def auth(username,password):
-    user=CustomUser.objects.get(username=username,password=password)
-    return user
+    try:
+      user=CustomUser.objects.get(username=username,password=password)
+      return user
+    
+    except CustomUser.DoesNotExist:
+        return None
 
 # @csrf_exempt
 class LoginView(APIView):
-    permission_classes=[AllowAny]
 
     def post(self,request,*args,**kwargs):
         print("HI")
         # print(request.data['username'])
-        # print(request.body)
-        username=request.data['username']
-        pwd=request.data['password']
-        print(username)
+        print(request.data)
+        try:
+           username=request.data['username']
+        except:
+            return Response({'error': 'Username MISSING'})
+        try:
+            pwd=request.data['password']
+        except:
+            return Response({'error': 'PASSWORD MISSING'})
+        print(type(username))
         print(pwd)
 
-        if username is None:
-            return Response({'error': 'MISSING credentials- Username MISSING'}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        if pwd is None:
-            return Response({'error': 'MISSING credentials-PASSWORD MISSING'}, status=status.HTTP_401_UNAUTHORIZED)
         user=auth(username,pwd)
         print(user)
-        if user:
-            login(request,user)
+        if user==None:
+            return Response({'error': 'NOT AN IITIAN'})
+        else:
+            login(request,user,backend='django.contrib.auth.backends.ModelBackend')
             print("LOGGED IN")
             serialize=User_Serializer(user)
             print(serialize.data)
             print(request.session.session_key)
             return Response({'user': serialize.data, 'session_id': request.session.session_key})
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         
 
         
--- new_frontend/vue.config.js
+++ new_frontend/vue.config.js
@@ -1,7 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const webpack = require('webpack');
+
+
+module.exports = {
   transpileDependencies: true,
   devServer: {
+    // Fixing issue with WDS disconnected and sockjs network error
     proxy: 'http://localhost:8000',
-  },
-})
+    host: '0.0.0.0',
+    public: '0.0.0.0:8080',
+    disableHostCheck: true,
+    // End of fix
+  }
+}
+
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     proxy: 'http://localhost:8000',
+//     port: 8080,
+//     public: 'https://localhost:5001'
+//   }
+// })
