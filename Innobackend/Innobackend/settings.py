ff8ed3344ad40d6d267e7db8fb11d52f5ccdaee7
Chitraksh <chitrakshsureka@gmail.com>
2024-01-02 17:08:20 +0530

    merge_resolve

--- Innobackend/Innobackend/settings.py
+++ Innobackend/Innobackend/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SOC',
         'USER': 'root',
-        'PASSWORD': 'JASL2005',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
