a3bfa04c61145ead4cf35914a55e1b022dbc8ddb
Chitraksh <chitrakshsureka@gmail.com>
2024-01-17 16:18:06 +0530

    idea_submission

--- new_frontend/constants.js
+++ new_frontend/constants.js
@@ -1,6 +1,7 @@
 export const proposalContractAddress =
-  "0x00b85211e10314efb2c4d3680ce7e0bac3753811";
-export const proposal_abi = [
+  "0x8f86403A4DE0BB5791fa46B8e795C547942fE4Cf";
+export const voteContractAddress = "0xBbF5A08dF0C8f2be530D3906a959f3F499c964Df";
+export const vote_abi = [
   {
     inputs: [
       {
@@ -14,17 +15,182 @@ export const proposal_abi = [
         type: "string",
       },
     ],
+    name: "proposal_votes",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "voted",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_project_id",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_user_id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_idea",
+        type: "string",
+      },
+    ],
+    name: "voting",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+export const proposal_abi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+    ],
+    name: "LogInformation",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "idea",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "problem_faced",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "project_name",
+            type: "string",
+          },
+          {
+            internalType: "address",
+            name: "proponentaddress",
+            type: "address",
+          },
+        ],
+        indexed: false,
+        internalType: "struct proposal.IDEA",
+        name: "i",
+        type: "tuple",
+      },
+    ],
+    name: "addedproposal",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "Idea",
+    outputs: [
+      {
+        internalType: "string",
+        name: "idea",
+        type: "string",
+      },
+      {
+        internalType: "bytes32",
+        name: "ideaid",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
     name: "Proposals",
     outputs: [
+      {
+        internalType: "string",
+        name: "idea",
+        type: "string",
+      },
       {
         internalType: "string",
         name: "problem_faced",
         type: "string",
       },
       {
-        internalType: "string",
-        name: "message",
-        type: "string",
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
       },
       {
         internalType: "string",
@@ -63,9 +229,9 @@ export const proposal_abi = [
         type: "string",
       },
       {
-        internalType: "string",
-        name: "_message",
-        type: "string",
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
       },
       {
         internalType: "address",
@@ -81,14 +247,86 @@ export const proposal_abi = [
   {
     inputs: [
       {
+        internalType: "uint8",
+        name: "project_id",
+        type: "uint8",
+      },
+    ],
+    name: "getIdea",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "idea",
+            type: "string",
+          },
+          {
+            internalType: "bytes32",
+            name: "ideaid",
+            type: "bytes32",
+          },
+        ],
+        internalType: "struct proposal.ideas[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "project_id",
+        type: "uint256",
+      },
+    ],
+    name: "getProjectProposal",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "idea1",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "problem_faced1",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "amount1",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "project_name1",
+            type: "string",
+          },
+        ],
+        internalType: "struct proposal.IDEAS[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
         internalType: "uint256",
         name: "project_id",
         type: "uint256",
       },
       {
-        internalType: "string",
-        name: "_idea",
-        type: "string",
+        internalType: "bytes32",
+        name: "_ideaid",
+        type: "bytes32",
       },
     ],
     name: "remove_proposal",
--- new_frontend/src/store/modules/idea.js
+++ new_frontend/src/store/modules/idea.js
@@ -1,5 +1,12 @@
 import { ethers } from "ethers";
-import { proposal_abi, proposalContractAddress } from "../../../constants.js";
+const { stringify } = require("json-bigint");
+
+import {
+  proposal_abi,
+  proposalContractAddress,
+  voteContractAddress,
+  vote_abi,
+} from "../../../constants.js";
 const idea_module = {
   namespaced: true,
   state: {
@@ -7,10 +14,14 @@ const idea_module = {
     problem: "",
     idea: "",
     amount: 0,
+    idea_detail: [],
   },
   mutations: {
     submitInfo(state) {
       console.log(state.idea);
+    },
+    setIdeas(state, ideas) {
+      state.idea_detail = stringify(Object.values(ideas));
     },
   },
   actions: {
@@ -20,15 +31,41 @@ const idea_module = {
         const accounts = await window.ethereum.request({
           method: "eth_requestAccounts",
         });
-        const provider = new ethers.BrowserProvider(window.ethereum);
+        const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
         const signer = await provider.getSigner();
         const contract = new ethers.Contract(
           proposalContractAddress,
           proposal_abi,
           signer
         );
+        // const contract2 = new ethers.Contract(
+        //   proposalContractAddress,
+        //   proposal_abi,
+        //   provider
+        // );
+        contract.on("*", (event) => {
+          //console.log(message);
+          console.log(event.log);
+        });
         try {
           console.log(rootState.e.cur_Selected.pk);
+          console.log(state.project);
+          console.log(state.idea);
+          console.log(state.problem);
+          console.log(state.amount);
+          console.log(accounts[0]);
+          // const demoTransaction = await contract.demo(8, {
+          //   gasLimit: 8000000,
+          //   gasPrice: ethers.parseUnits("30", "gwei"),
+          // });
+
+          // // Wait for the transaction to be mined
+          // await demoTransaction.wait();
+
+          // Query the updated value
+          // const updatedValue = await contract.getUpdatedValue.staticCall();
+          // console.log("Updated Value:", updatedValue);
+          console.log(contract.add_proposal);
           const proposalTransaction = await contract.add_proposal(
             rootState.e.cur_Selected.pk,
             state.project,
@@ -37,24 +74,87 @@ const idea_module = {
             state.amount,
             accounts[0],
             {
-              gasLimit: 300000,
+              gasLimit: 8000000,
               gasPrice: ethers.parseUnits("30", "gwei"),
             }
           );
           await proposalTransaction.wait();
+          console.log(proposalTransaction);
           console.log("Transaction mined");
+
           //await this.listenproposal(proposaladd, provider);
-          console.log(
-            contract.Proposals[rootState.e.cur_Selected.project_topic][
-              state.idea
-            ]
+          // const myproblem = await contract.Proposals(
+          //   rootState.e.cur_Selected.pk
+          // );
+          // console.log(myproblem);
+          console.log(rootState.e.cur_Selected.pk);
+          const projectProposals = await contract.getProjectProposal.staticCall(
+            1
           );
+          // await projectProposals.wait();
+          if (projectProposals.length > 0) {
+            console.log(projectProposals);
+            console.log("Horjgnjerg");
+          } else {
+            console.log("No proposals found for the specified project.");
+          }
         } catch (error) {
           console.log(error);
         }
       }
     },
+    async Vote_on_Idea({ state, rootState }) {
+      console.log("starting voting");
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(
+          voteContractAddress,
+          vote_abi,
+          signer
+        );
+        const voteTx = await contract.voting();
+      }
+    },
+    async getIdeas({ state, rootState, commit }) {
+      console.log("hiii");
+      const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
+      //const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        proposalContractAddress,
+        proposal_abi,
+        provider
+      );
+      //console.log(await contract.Proposals);
+      // const myproblem = await contract.Proposals(rootState.e.cur_Selected.pk);
+      // console.log(myproblem);
 
+      //const projectIdea = await contract.getIdea(rootState.e.cur_Selected.pk);
+      const projectProposals = await contract.getProjectProposal.staticCall(
+        rootState.e.cur_Selected.pk
+      );
+      console.log(projectProposals);
+      commit("setIdeas", projectProposals);
+
+      // for (let i = 0; i < projectProposals.length; i++) {
+      //   const curidea = await projectProposals[i];
+      //   // const curideaid = await projectIdea[i].ideaid;
+      //   // state.idea_names.push(curidea);
+      //   state.idea_detail.push(curidea);
+      // }
+      console.log(state.idea_detail);
+      // for (let i = 0; i < contract.Idea.length; i++) {
+      //   const curidea = await contract.Idea[i].idea;
+      //   const curideaid = await contract.Idea[i].ideaid;
+      //   state.idea_names.push(curidea);
+      //   state.idea_ids.push(curideaid);
+      // }
+      console.log("hi");
+      //console.log(state.idea_names);
+    },
     // listenproposal(proposaladd, provider) {
     //   return new Promise((resolve) => {
     //     provider.once(proposaladd.hash, (proposalReceipt) => {
